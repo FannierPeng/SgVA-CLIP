93cea7aa891da8e390b337b5c444a63772fbeef1
FannierPeng <40656312+FannierPeng@users.noreply.github.com>
2025-05-12 22:05:00 +0800

    Update eurosat.py

--- data/eurosat.py
+++ data/eurosat.py
@@ -24,7 +24,8 @@ sys.path.append("..")
 from utils import Datum
 
 # Set the appropriate paths of the datasets here.
-_IMAGE_DATASET_DIR = '/userhome/CLIP/data/eurosat/'
+OLD_IMAGE_DATASET_DIR = '/userhome/CLIP/data/eurosat/'
+_IMAGE_DATASET_DIR = ''
 
 NEW_CNAMES = {
     "AnnualCrop": "Annual Crop Land",
@@ -300,6 +301,8 @@ class EuroSAT(data.Dataset):
         impath, label = self.impaths[index], self.labels[index]
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
+        if 'userhome' in impath:
+            impath = impath.replace(OLD_IMAGE_DATASET_DIR, _IMAGE_DATASET_DIR)
         img = Image.open(impath)
         img = img.convert('RGB')
         img = self.transform(img)
